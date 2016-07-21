8db197d8cd09b734618c08c6ebaacc8bb6548f29
Maximilian Stroh <Hisako1337@gmail.com>
2016-07-21 18:11:05 +0200

    included rendering options for use

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// the basic building block is the handlebars rendering engine
-const hbs = require('handlebars');
-
-// more functionality directly added via custom plugins from ./lib
-require('./lib/numbro-helpers')(hbs); // numbers & currencies
-require('./lib/moment-helpers')(hbs); // dates, times & durations
-require('./lib/l10n-helpers')(hbs); // localization
-
-// extend rendering with layout functionality
-const handlebarsLayouts = require('handlebars-layouts');
-handlebarsLayouts.register(hbs);
-
 /*
 const slinkHelper = require('hbs-slink-helper');
 
@@ -132,6 +120,20 @@ handlebars.registerHelper('dff', function(str, format, opts) {
 
 */
 
+// initializes and configures a custom handlebars instance
+function init(opts) {
+  // the basic building block is the handlebars rendering engine
+  const hbs = require('handlebars');
+  // more functionality directly added via custom plugins from ./lib
+  require('./lib/numbro-helpers')(hbs, opts); // numbers & currencies
+  require('./lib/moment-helpers')(hbs, opts); // dates, times & durations
+  require('./lib/l10n-helpers')(hbs, opts); // localization
+  // extend rendering with layout functionality
+  const handlebarsLayouts = require('handlebars-layouts');
+  handlebarsLayouts.register(hbs);
+  return hbs;
+}
+
 /**
 the following data is expected on the context:
 
@@ -145,16 +147,17 @@ class Renderer {
   /**
   @param {String} template the template
   @param {String} layout the optional layout
+  @param {Object} settings and localization data for the renderer
   */
-  constructor(template, layout) {
+  constructor(template, layout, opts) {
+    this.hbs = init(opts);
     if (layout) {
-      hbs.registerPartial('layout', layout);
+      this.hbs.registerPartial('layout', layout);
     }
-    this.template = hbs.compile(template);
+    this.template = this.hbs.compile(template);
   }
 
   /**
-  @param {String} template
   @param {Object} context
   @return {String} html
   */
