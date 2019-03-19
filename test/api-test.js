d0f7f9e71d67f80aaf40c9397c425231d0088373
akumar <arun.kumar@n-fuse.co>
2019-03-19 13:52:07 +0100

    up time_stamp for tests.

--- test/api-test.js
+++ test/api-test.js
@@ -59,43 +59,43 @@ describe('The README examples', () => {
   it('should pass the formatting example', () => {
     const tpl = '<p>You paid {{nfc price cs="$"}} on {{df date}}</p>';
     const renderer = new Renderer(tpl);
-    const ts = '07-22-2018 13:37:00';
+    const ts = '03-19-2019 13:37:00';
     const format = 'MM-DD-YYYY HH:mm:ss';
     const tz = moment.tz.guess();
     const yesterday = moment.parseZone(ts, format, tz);
     const result = renderer.render({ price: 1.99, date: yesterday });
-    const expectedResult = '<p>You paid $1.99 on 07/22/2018</p>';
+    const expectedResult = '<p>You paid $1.99 on 03/19/2019</p>';
     result.should.equal(expectedResult);
   });
 
   it('should pass the formatting example directly from the template', () => {
     const tpl = '<p>You paid {{nfc price cs="$"}} on {{dff date format=\'YYYY-DD-MM HH:mm:ss\'}}</p>';
     const renderer = new Renderer(tpl);
-    const ts = '2018-07-22 13:37:00';
+    const ts = '2019-03-19 13:37:00';
     const result = renderer.render({ price: 1.99, date: ts });
-    const expectedResult = '<p>You paid $1.99 on 2018-22-07 13:37:00</p>';
+    const expectedResult = '<p>You paid $1.99 on 2019-19-03 13:37:00</p>';
     result.should.equal(expectedResult);
   });
 
   it('should format with a custom timezone', () => {
     const tpl = '<p>You paid {{nfc price cs="€" csPos="postfix"}} on {{dff date format=\'YYYY-DD-MM HH:mm:ss\' timezone="Europe/Moscow"}}</p>';
     const renderer = new Renderer(tpl);
-    const ts = '2018-03-02T10:46:43.246Z';
+    const ts = '2019-03-19T10:46:43.246Z';
     const result = renderer.render({ price: 1.99, date: ts });
     // Moscow is 3 hours in advance of GMT
-    const expectedResult = '<p>You paid 1.99€ on 2018-02-03 13:46:43</p>';
+    const expectedResult = '<p>You paid 1.99€ on 2019-19-03 13:46:43</p>';
     result.should.equal(expectedResult);
   });
 
   it('should pass the formatting example with german locale', () => {
     const tpl = '<p>You paid {{nfc price cs="€" csPos="postfix"}} on {{df date}}</p>';
     const renderer = new Renderer(tpl, null, null, { locale: 'de_DE' });
-    const ts = '07-22-2018 13:37:00';
+    const ts = '03-19-2019 13:37:00';
     const format = 'MM-DD-YYYY HH:mm:ss';
     const tz = moment.tz.guess();
     const yesterday = moment.parseZone(ts, format, tz);
     const result = renderer.render({ price: 1.99, date: yesterday });
-    const expectedResult = '<p>You paid 1,99€ on 22.07.2018</p>';
+    const expectedResult = '<p>You paid 1,99€ on 19.03.2019</p>';
     result.should.equal(expectedResult);
   });
 });
